97bf1913bb50ed5611ebe5023bd2fb491080a640
Luiz Nörnberg (home) <luiz.nornberg@americansbitcoin.com>
2021-11-24 18:01:35 -0300

    WIP: refatoração no fluxo de carga dos elementos e na representação do para selecionado. base para seleção do ano.

--- src/App.js
+++ src/App.js
@@ -17,6 +17,13 @@ const View = styled.div`
   border: 1px red solid;
 `;
 
+const Button = styled.button`
+  min-width: 100px;
+  min-height: 40px;
+  border-radius: 5px;
+  background-color: ${(p) => (p.active ? "orange" : "white")};
+`;
+
 // https://colorbrewer2.org/#type=diverging&scheme=RdYlGn&n=10
 const coverageColors = [
   "#a50026", // 0 = vermelho, 0%
@@ -34,56 +41,80 @@ const coverageColors = [
 const App = () => {
   const [svgMap, setSvgMap] = useState(null);
   const [colorData, setColorData] = useState([]);
-  const [svgUrl, setSvgUrl] = useState(
-    "https://servicodados.ibge.gov.br/api/v3/malhas/paises/BR?formato=image/svg+xml&qualidade=intermediaria&intrarregiao=UF"
-  );
+  const [year, setYear] = useState(2010);
+  const [mapId, setMapId] = useState(null);
 
   useEffect(() => {
-    console.log("[App.useEffect]");
-    let coverageData = [];
-    Promise.all([fetch(svgUrl), fetch(process.env.PUBLIC_URL + "/mun_data_bcg.csv")]).then((allData) => {
-      const svgResponse = allData[0];
-      svgResponse.text().then((text) => {
-        console.log("[App.useEffect][setSvgMap]");
-        setSvgMap(text);
-        const csvResponse = allData[1];
-        csvResponse.text().then((text) => {
-          text = text.split("\n");
-          delete text[0];
-          const colorIndex = (coverage) => (coverage === 0 ? 0 : Math.trunc((coverage - 1) / 10));
-          coverageData = text.map((entry) => {
-            const obj = entry.split(",");
-            return { id: obj[0], year: obj[4], coverage: obj[5], color: coverageColors[colorIndex(obj[5])] };
-          });
-          console.log("[App.useEffect][setColorData]");
-          setColorData(coverageData);
+    console.log("[App.useEffect 1]");
+    fetch(process.env.PUBLIC_URL + "/mun_data_bcg.csv").then((csvResponse) => {
+      csvResponse.text().then((text) => {
+        text = text.split("\n");
+        delete text[0];
+        const colorIndex = (coverage) => (coverage === 0 ? 0 : Math.trunc((coverage - 1) / 10));
+        const coverageData = text.map((entry) => {
+          const obj = entry.split(",");
+          return { id: obj[0], year: obj[4], coverage: obj[5], color: coverageColors[colorIndex(obj[5])] };
         });
+        console.log("[App.useEffect 1][setColorData]");
+        setColorData(coverageData);
       });
     });
-  }, [svgUrl]);
+  }, []);
+
+  useEffect(() => {
+    console.log("[App.useEffect 2]");
+    //console.log("[App.useEffect 2] Clearing current svg...");
+    //setSvgMap(null);
+    const svgUrl = generateSvgUrl(mapId);
+    fetch(svgUrl).then((svgResponse) => {
+      svgResponse.text().then((text) => {
+        console.log("[App.useEffect 2][setSvgMap]");
+        setSvgMap(text);
+      });
+    });
+  }, [mapId]);
 
   const handleMapClick = (id) => {
-    if (id === "2401453") {
-      console.log("[App.handleMapClick] Setting svgURL for BRASIL");
-      setSvgUrl(
-        "https://servicodados.ibge.gov.br/api/v3/malhas/paises/BR?formato=image/svg+xml&qualidade=intermediaria&intrarregiao=UF"
-      );
-    } else if (Number(id) < 100) {
-      console.log("[App.handleMapClick] Setting svgURL for UF " + id);
-      setSvgUrl(
-        "https://servicodados.ibge.gov.br/api/v3/malhas/estados/24?formato=image/svg+xml&qualidade=minima&intrarregiao=municipio"
-      );
+    id = Number(id);
+    console.log("[App.handleMapClick] Clicked id=" + id);
+    if (id < 100) {
+      setMapId(id);
     } else {
-      console.log("[App.handleMapClick] Ignored city " + id);
+      console.log("Show city data for id=", id);
     }
   };
 
-  console.log("[App.render]");
+  const handleBackClick = (e) => {
+    console.log("[App.handleBackClick] ");
+    setMapId(null);
+  };
+
+  const handleYearClick = (year) => (e) => {
+    console.log("[App.handleYearClick] ", year, e.target);
+    setYear(year);
+  };
+
+  const generateSvgUrl = (id) =>
+    !id
+      ? "https://servicodados.ibge.gov.br/api/v3/malhas/paises/BR?formato=image/svg+xml&qualidade=intermediaria&intrarregiao=UF"
+      : `https://servicodados.ibge.gov.br/api/v3/malhas/estados/${id}?formato=image/svg+xml&qualidade=minima&intrarregiao=municipio`;
+
+  console.log("[App.render] ");
   return (
     <Container>
       <View>Indicadores de saúde do Brasil</View>
       <View>Cobertura da vacinação BCG</View>
-      <View>Slider</View>
+      <View>
+        <Button onClick={handleBackClick}>Voltar</Button>
+      </View>
+      <View>
+        <Button onClick={handleYearClick(2010)} active={year === 2010}>
+          2010
+        </Button>
+        <Button onClick={handleYearClick(2011)} active={year === 2011}>
+          2011
+        </Button>
+      </View>
       <MapView SvgComp={svgMap} colorData={colorData} onClick={handleMapClick}></MapView>
     </Container>
   );
